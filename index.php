3bffe5b8057c43b2c153196dcca555d2b70f8f46
use-x <emil-asmussen@use-x.com>
2019-02-14 14:28:31 +0100

    1. commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<meta charset="UTF-8">
+<title>Standard Template on Use-X</title>
+<link rel="stylescheet" type="text/css" href="./styles.css">
+</head>
+<body>
+
+Hallo Emil!
+
+</body>
+</html>
+
+
+
