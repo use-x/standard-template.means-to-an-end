226eadc754089c548ffd298d73f74a5aa6908c35
use-x <emil-asmussen@use-x.com>
2019-02-14 14:56:59 +0100

    2. commit

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="UTF-8">
 <title>Standard Template on Use-X</title>
 <link rel="stylescheet" type="text/css" href="./styles.css">
